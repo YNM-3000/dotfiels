d49c31052d8f62d79fb1b530964644f67a37add7
YNM3000 <742702052@qq.com>
2023-11-21 11:32:08 +0800

    [patch]update lualine config

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -3,29 +3,10 @@ if not lualine_ok then
 	return
 end
 
-local lualine_nightfly = require("lualine.themes.nightfly")
-
-local new_colors = {
-	blue = "#89B4FA",
-	green = "#A6E3A1",
-	violet = "#F38BA8",
-	yellow = "#F9E2AF",
-	black = "#000000",
-}
-
-lualine_nightfly.normal.a.bg = new_colors.blue
-lualine_nightfly.insert.a.bg = new_colors.green
-lualine_nightfly.visual.a.bg = new_colors.violet
-lualine_nightfly.command = {
-	a = {
-		gui = "bold",
-		bg = new_colors.yellow,
-		fg = new_colors.black,
-	},
-}
-
 lualine.setup({
 	options = {
 		theme = "catppuccin",
+		section_separators = "",
+		component_separators = "|",
 	},
 })
